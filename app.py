f110f69b750d62ca3be9fefd58b774123ef27b07
Musa Azeem <mmazeem@email.sc.edu>
2022-04-15 16:18:44 -0400

    work

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
 import flask
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, flash, url_for
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = "raw/"
+INPUT_NAME = "file_in"
+# ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", input_name = INPUT_NAME)
+
+@app.route("/process-file", methods=["POST"])
+def process_file():
+    if request.method == 'POST':
+        if INPUT_NAME not in request.files:
+            return redirect('/fail-input')    # no file
+        file = request.files[INPUT_NAME]
+        if file.filename == '':
+            return redirect('/fail-input')
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect('/success') 
+    return redirect('/')
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+@app.route("/fail-input")
+def fail():
+    return ("fail")
---
+++ scripts/to_upper_case.py
@@ -0,0 +1,11 @@
+def file_to_upper(file):
+    file_path = f'raw/{file}'
+    if not open(file_path):
+        print('file does not exist')
+        return(-1)
+    filelines = open(file).read().splitlines()
+    filelines = [line.upper() for line in filelines]
+    new_file = f'processed/{file}'
+    with open(new_file, 'w') as f:
+        for line in filelines:
+            f.write(line+"\n")
